00f970e860209651ef74735073dbdf758205181d
taka66 <taka66.iphone@gmail.com>
2016-05-15 19:42:55 +0900

    Add sample code to learn about props.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-ReactDOM.render(<h1>Hello world</h1>, document.body);
+var md5 = require('MD5');
+var GRAVATAR_URL="http://gravatar.com/avatar";
+
+var USERS = [
+  {id:1, name:'takahiro fujii', email:'takahiro.fujii@rakuten.com'}
+];
+
+var Gravatar = React.createClass({
+  render(){
+    var size = 36;
+    var hash = md5(this.props.email);
+    var url =  `${GRAVATAR_URL}/${hash}?s=${size*2}`;
+    return <img src={url} width={size} />;
+  }
+});
+
+var App = React.createClass({
+  render() {
+    var users = USERS.map((user) => {
+      return <li><Gravatar email={user.email} /> {user.name}</li>
+    });
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>{users}</ul>
+      </div>
+    );
+  }
+});
+
+var element = <App />
+
+ReactDOM.render(element, document.body);
